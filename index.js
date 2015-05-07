8fc74c499d7135440e80160fd260b3de431231db
jaywunder <jaywunder@gmail.com>
2015-05-07 15:00:48 -0400

    working chat application

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-//SETUP EXPRESS ERVER
+////////////////////////
+//SETUP EXPRESS SERVER//
+////////////////////////
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
@@ -10,19 +12,42 @@ server.listen(port, function() {
   console.log('Server listening at port %d', port);
 });
 
-//ROUTING
+///////////
+//ROUTING//
+///////////
 app.use(express.static(__dirname + '/public'));
-
+//Use app.get if you're going to route to a page that can't be served statically
 // app.get('/', function(req, res){
 //   //req means "request" and res mean "result"
 // });
 
-//Chat room
+
+/////////////
+//CHAT ROOM//
+/////////////
 var usernames = {};
 var numUsers = 0;
 
+
+//////////////////
+//EVENT HANDLING//
+//////////////////
 io.on('connection', function (socket) {
+  console.log('a user connected');
+  //emit events
   socket.broadcast.emit('hey', {
     data: 'hello socket'
+  });
+
+  socket.emit('chat message', 'testing chat message functionality');
+
+  //bind to events
+  socket.on('disconnect', function(){ //disconnect event
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg){ // send message event
+    socket.broadcast.emit('chat message', msg);
+    socket.emit('chat message', msg);
   });
 });
--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,14 @@ var socket = io();
 socket.on('hey', function (data) {
   console.log(data.data);
 });
+
+$('form').submit(function(e){
+  e.preventDefault();
+  console.log('sumbitted the form');
+  socket.emit('chat message', $('#m').val());
+  $('#m').val('');
+});
+
+socket.on('chat message', function(msg){
+  $('#messages').append($('<li>').text(msg));
+});
