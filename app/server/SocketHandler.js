3057b5fd772389038c34a7ec403f4cb6ffa1487f
jaywunder <jaywunder@gmail.com>
2015-05-07 20:48:59 -0400

    moved socket stuff to a SocketHandler function
    added app/server/SocketHandler.js

---
+++ app/server/SocketHandler.js
@@ -0,0 +1,21 @@
+module.exports = function(socket) {
+  console.log('a user connected');
+  //emit events
+  socket.broadcast.emit('hey', {
+    data: 'hello socket'
+  });
+
+  //bind to events
+  socket.on('disconnect', function(){ //disconnect event
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', function(msg){ // send message event
+    socket.broadcast.emit('chat message', msg);
+    socket.emit('chat message', msg);
+  });
+
+  socket.on('keypress',  function(event) {
+    // body...
+  });
+};
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 "use strict";
+//CONSTANTS//
+const port = process.env.PORT || 3001;
 
 //SETUP EXPRESS SERVER//
 let express = require('express');
 let app = express();
 let server = require('http').createServer(app);
 let io = require('socket.io')(server);
-const port = process.env.PORT || 3001;
+let SocketHandler = require('./app/server/SocketHandler');
 
 //listen to the 'port'
 server.listen(port, function() {
@@ -21,23 +23,5 @@ app.use(express.static(__dirname + '/public'));
 
 //EVENT HANDLING//
 io.on('connection', function (socket) {
-  console.log('a user connected');
-  //emit events
-  socket.broadcast.emit('hey', {
-    data: 'hello socket'
-  });
-
-  //bind to events
-  socket.on('disconnect', function(){ //disconnect event
-    console.log('user disconnected');
-  });
-
-  socket.on('chat message', function(msg){ // send message event
-    socket.broadcast.emit('chat message', msg);
-    socket.emit('chat message', msg);
-  });
-
-  socket.on('keypress',  function(event) {
-    // body...
-  });
+  new SocketHandler(socket);
 });
