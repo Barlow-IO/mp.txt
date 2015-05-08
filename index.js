ce6e96a07ed4516955c16720e941a654ecf59256
jaywunder <jaywunder@gmail.com>
2015-05-07 20:07:18 -0400

    removed ugly comments

--- index.js
+++ index.js
@@ -1,37 +1,25 @@
 "use strict";
 
-////////////////////////
 //SETUP EXPRESS SERVER//
-////////////////////////
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 3001;
+let express = require('express');
+let app = express();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+const port = process.env.PORT || 3001;
 
 //listen to the 'port'
 server.listen(port, function() {
   console.log('Server listening at port %d', port);
 });
 
-///////////
 //ROUTING//
-///////////
 app.use(express.static(__dirname + '/public'));
 //Use app.get if you're going to route to a page that can't be served statically
 // app.get('/', function(req, res){
 //   //req means "request" and res mean "result"
 // });
 
-/////////////
-//CHAT ROOM//
-/////////////
-var usernames = {};
-var numUsers = 0;
-
-//////////////////
 //EVENT HANDLING//
-//////////////////
 io.on('connection', function (socket) {
   console.log('a user connected');
   //emit events
@@ -47,5 +35,9 @@ io.on('connection', function (socket) {
   socket.on('chat message', function(msg){ // send message event
     socket.broadcast.emit('chat message', msg);
     socket.emit('chat message', msg);
+  });
+
+  socket.on('keypress',  function(event) {
+    // body...
   });
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,26 @@
 var socket = io();
 
-socket.on('hey', function (data) {
+socket.on('hey', function(data) {
   console.log(data.data);
 });
 
 $('form').submit(function(e){
   e.preventDefault();
-  console.log('sumbitted the form');
   socket.emit('chat message', $('#m').val());
   $('#m').val('');
+});
+
+$(window).keypress(function(e) {
+  event = {
+    type: e.type,
+    timeStamp: e.timeStamp,
+    target: e.target,
+    shiftKey: e.shiftKey,
+    metaKey: e.metaKey,
+    ctrlKey: e.ctrlKey,
+    altKey: e.altKey
+  };
+  // socket.emit('keypress', event);
 });
 
 socket.on('chat message', function(msg){
