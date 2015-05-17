cefce68aaa7ec240ca7f2ad08aa7c96df8b3862c
jaywunder <jaywunder@gmail.com>
2015-05-17 16:54:19 -0400

    added a few models

---
+++ app/models/tile.js
@@ -0,0 +1,5 @@
+module.exports = class {
+  constructor(char) {
+    this.char = char;
+  }
+};
--- app/server/db.js
+++ app/server/db.js
@@ -1,7 +1,8 @@
 var low = require('lowdb');
+var Tile = require('../models/tile');
 
 var db = low('db.json');
-db('posts').push({ title: 'lowdb is awesome'});
 
+db('tiles').push(new Tile('#'));
 db.save('db.json');
 module.exports = db;
